0833ff8861d71f49dfa3f970431723a29a0e57ba
Monkey-man9988 <prajwald285@gmail.com>
2025-05-11 15:48:08 -0400

    Add files via upload

---
+++ RobloxCheat.lua
@@ -0,0 +1,19 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+
+local Window = Rayfield:CreateWindow({
+   Name = "Cheat Window",
+   Icon = 0, 
+   LoadingTitle = "Cheat",
+   LoadingSubtitle = "by MonkeyMan",
+   Theme = "Bloom",
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = false,
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil,
+      FileName = "Skibidi"
+   },
+})
